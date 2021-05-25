c084a2f41c747fdef6c11600dc2acc79b92e4427
Kevin Moy <kmoy14@stanford.edu>
2021-05-25 16:59:09 -0700

    plotting for problem 2

--- HW3/problem_2/mpc.py
+++ HW3/problem_2/mpc.py
@@ -1,6 +1,6 @@
-#%%
+#%% Imports
 import numpy as np
-import scipy as sp
+import scipy.linalg as sla
 import cvxpy as cvx
 import copy
 import matplotlib.pyplot as plt
@@ -11,7 +11,7 @@ B = np.array([[0.],[1.]])
 Q = np.eye(2)
 R = 0.01*np.eye(1)
 
-#%% TODO: Implement one step of MPC using cvxpy
+#%% Implement one step of MPC using cvxpy
 # Input: x(t), A, B, Q, R, P, N, x_bar, u_bar, X_f
 # Output: u(t)
 
@@ -48,36 +48,34 @@ def opt_finite_traj(A, B, Q, R, P, N, x_bar, u_bar, x0, X_f0=False):
     # Note this is either -Inf or Inf if infeasible or unbounded
     return converged, u_new
 
-#%% TODO: Implement entire receding horizon control
-
-def mpc(A, B, Q, R, P, N, x_bar, u_bar, x0):
+#%% Implement entire receding horizon control
+def mpc(A, B, Q, R, P, N, x_bar, u_bar, x0, X_f0=False):
     x = copy.deepcopy(x0)
     x_hist = copy.deepcopy(x0)
     feas = True
-    converged, u = opt_finite_traj(A, B, Q, R, P, N, x_bar, u_bar, x0)
+    converged, u = opt_finite_traj(A, B, Q, R, P, N, x_bar, u_bar, x0, X_f0)
     if converged:
         x1 = A @ x0 + B @ u[0]
         x_hist = np.vstack([x_hist, x1])
+        round = 0
+        eps = 1e-5
+        # TODO: Collapse this into a while function
+        for i in range(100):
+            if (np.linalg.norm(x-x1) > eps):
+                # print("round: %s, x update: %s" % (round, x))
+                x = x1
+                converged, u = opt_finite_traj(A, B, Q, R, P, N, x_bar, u_bar, x)
+                if converged:
+                    x1 = A @ x + B @ u[0]
+                    x_hist = np.vstack([x_hist, x1])
+                else:
+                    feas = False
+                    break
+                round += 1
+            else:
+                break
     else:
         feas = False
-
-    round = 0
-    eps = 1e-5
-    # TODO: Collapse this into a while function
-    for i in range(100):
-        if (np.linalg.norm(x-x1) > eps):
-            print("round: %s, x update: %s" % (round, x))
-            x = x1
-            converged, u = opt_finite_traj(A, B, Q, R, P, N, x_bar, u_bar, x)
-            if converged:
-                x1 = A @ x + B @ u[0]
-                x_hist = np.vstack([x_hist, x1])
-            else:
-                feas = False
-                break
-            round += 1
-        else:
-            break
 
     if feas == False:
         feasible = False
@@ -92,13 +90,12 @@ x_bar = 5
 u_bar = 0.5
 N = 3
 P = np.eye(2)
-R = 10.0*np.eye(1)
+Rb = 10.0*np.eye(1)
 
-feas1, x_hist1 = mpc(A, B, Q, R, P, N, x_bar, u_bar, np.array([-4.5, 2]))
-feas2, x_hist2 = mpc(A, B, Q, R, P, N, x_bar, u_bar, np.array([-4.5, 3]))
+feas1, x_hist1 = mpc(A, B, Q, Rb, P, N, x_bar, u_bar, np.array([-4.5, 2]))
+feas2, x_hist2 = mpc(A, B, Q, Rb, P, N, x_bar, u_bar, np.array([-4.5, 3]))
 
-#%% Plot results
-
+# Plot results
 plt.plot(x_hist1[:,0], x_hist1[:,1], marker='o')
 plt.plot(x_hist2[:,0], x_hist2[:,1],  marker='o')
 plt.axis('square')
@@ -106,18 +103,50 @@ plt.xlabel('x1')
 plt.ylabel('x2')
 plt.grid()
 plt.axis('square')
+
+#%% Part (c), (d), (e), (f)
+#TODO: Construct function to find feasible points
+x_bar = 10.0
+u_bar = 1.0
+N = 6
+
+P_inf = sla.solve_discrete_are(A, B, Q, R)
+
+# discretize state space
+x1 = np.linspace(-10, 10, 21)
+x2 = np.linspace(-10, 10, 21)
+
+# Find feasible points
+x_feas = np.array([0, 0])
+eps = 1e-4
+for i in x1:
+    for j in x2:
+        coords = np.array([i, j])
+        feas, x_hist = mpc(A, B, Q, R, P_inf, N, x_bar, u_bar, coords)
+        if feas & (np.linalg.norm(x_hist1[-1] - np.zeros(2)) < eps):
+            x_feas = np.vstack([x_feas, coords])
+
+# %%
+plt.scatter(x_feas[:,0], x_feas[:,1])
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.grid()
+plt.xlim(-10,10)
+plt.ylim(-10,10)
+plt.title(r'$N = {}, X_f = \mathbb{{R}}^2$'.format(N))
+# plt.title(r'$N = {}, X_f = 0$'.format(N))
+# %% Part (h)
+
+x0 = np.array([-0.1, 0.01])
+feas1, x_hist1 = mpc(A, B, Q, R, P, 2, x_bar, u_bar, x0, True)
+feas2, x_hist2 = mpc(A, B, Q, R, P, 3, x_bar, u_bar, x0, True)
+feas3, x_hist3 = mpc(A, B, Q, R, P, 6, x_bar, u_bar, x0, True)
+feas3, x_hist4 = mpc(A, B, Q, R, P, 12, x_bar, u_bar, x0, True)
+
 #%%
-t = cvx.Variable()
-
-# An infeasible problem.
-prob = cvx.Problem(cvx.Minimize(t), [t >= 1, t <= 0])
-prob.solve()
-print("status:", prob.status)
-print("optimal value", prob.value)
-
-# An unbounded problem.
-prob = cvx.Problem(cvx.Minimize(t))
-prob.solve()
-print("status:", prob.status)
-print("optimal value", prob.value)
+plt.plot(x_hist1[:,0], x_hist1[:,1], marker='o')
+plt.plot(x_hist2[:,0], x_hist2[:,1],  marker='o')
+plt.plot(x_hist3[:,0], x_hist3[:,1], marker='o')
+plt.plot(x_hist4[:,0], x_hist4[:,1],  marker='o')
+plt.legend(['N=2', 'N=3', 'N=6', 'N=12'])
 # %%
